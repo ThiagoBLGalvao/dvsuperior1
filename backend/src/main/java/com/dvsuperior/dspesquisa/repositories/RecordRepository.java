987df2b29631ff604abeb69c3064e7622000444d
ThiagoBLGalvao <thiagoblgalvao@hotmail.com>
2020-09-17 18:40:02 -0300

    GET records

--- backend/src/main/java/com/dvsuperior/dspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/dvsuperior/dspesquisa/repositories/RecordRepository.java
@@ -1,11 +1,23 @@
 package com.dvsuperior.dspesquisa.repositories;
 
+import com.dvsuperior.dspesquisa.dto.RecordDTO;
 import com.dvsuperior.dspesquisa.entities.Game;
 import com.dvsuperior.dspesquisa.entities.Record;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.util.List;
 
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
 
+    @Query("SELECT obj FROM Record obj WHERE " +
+            "(coalesce(:min, null) IS NULL OR obj.moment >= :min) AND " +
+            "(coalesce(:max, null) IS NULL OR obj.moment <= :max)")
+    Page<Record> findByMoments(Instant min, Instant max, Pageable pageable);
 }
