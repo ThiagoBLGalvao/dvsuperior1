0f3af71b406f63e22677c428951e01e3cbe9e161
ThiagoBLGalvao <thiagoblgalvao@hotmail.com>
2020-09-20 01:27:06 -0300

    Adicionando componente Header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>Hello World UwU!!</h1>
+    <div>
+      <Header />
+      <h1>Hello World UwU!!</h1>
+    </div>
+
   );
 }
 
